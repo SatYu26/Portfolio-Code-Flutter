8532783bc04e0bc112c5f41c65893dbeaf23d7a6
SatYu26 <goyalsatyam8@gmail.com>
2020-09-08 00:44:22 +0530

    navigation drawer onTap open added

--- lib/widgets/navigation_bar/navigation_bar_mobile.dart
+++ lib/widgets/navigation_bar/navigation_bar_mobile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:satyam_website/widgets/navigation_bar/navbar_logo.dart';
+import 'package:satyam_website/widgets/nav_drawer/navigation_drawer.dart';
 
 
 class NavigationBarMobile extends StatelessWidget {
@@ -15,7 +16,7 @@ class NavigationBarMobile extends StatelessWidget {
         children: <Widget>[
           IconButton(
             icon: Icon(Icons.menu),
-            onPressed: () {},
+            onPressed: () {Scaffold.of(context).openDrawer();},
           ),
           NavBarLogo()
         ],
